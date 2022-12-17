6d17a82479c380fc89381c8e36f99b2f26876b56
HudahanAltn <dev.hudahanaltun@gmail.com>
2022-12-17 21:02:20 +0300

    Initial Commit

---
+++ MapApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapApplication
+//
+//  Created by Hüdahan Altun on 17.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
